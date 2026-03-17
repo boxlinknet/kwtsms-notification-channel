5b91e95f7676de6b83bc3da0b9890aa6f4f20274
M Alhassoun <mo@boxlink.net>
2026-03-17 09:40:27 +0300

    Remove array key alignment in config

--- config/kwtsms.php
+++ config/kwtsms.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'username'  => env('KWTSMS_USERNAME', ''),
-    'password'  => env('KWTSMS_PASSWORD', ''),
-    'sender'    => env('KWTSMS_SENDER', 'KWT-SMS'),
+    'username' => env('KWTSMS_USERNAME', ''),
+    'password' => env('KWTSMS_PASSWORD', ''),
+    'sender' => env('KWTSMS_SENDER', 'KWT-SMS'),
     'test_mode' => env('KWTSMS_TEST_MODE', false),
 ];
